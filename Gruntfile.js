56c6224236c351ba5802107814513c714fb49a81
Devin Weaver <suki@tritarget.org>
2013-09-03 21:09:09 -0400

    Adds Gruntfile.js

---
+++ Gruntfile.js
@@ -0,0 +1,62 @@
+module.exports = function (grunt) {
+  var
+    build_dir = "build/",
+    pkg       = require("./package.json");
+
+  function compressJsFile(file) {
+    var UglifyJS = require("uglify-js2");
+    var result   = UglifyJS.minify(file);
+    return result.code;
+  }
+
+  function compressCssFile(file) {
+    var UglifyCSS = require("uglifycss");
+    return UglifyCSS.processFiles([file]);
+  }
+
+  grunt.initConfig({ pkg: pkg });
+
+  grunt.registerTask("default", ["build"]);
+
+  grunt.registerTask("build:compileStandAlone",
+    "Builds jasmine-all.js",
+    function() {
+      var output = grunt.template.process(
+        grunt.file.read("jasmine-all.jst"),
+        { data: {
+          jasmineVersion:      pkg.version,
+          jasmineCss:          compressCssFile("lib/jasmine.css"),
+          jasmineHtmlReporter: grunt.file.read("lib/jasmine-html.js"),
+          jasmineCore:         grunt.file.read("lib/jasmine.js")
+        } }
+      );
+
+      grunt.file.write(build_dir + "jasmine-all.js", output);
+    }
+  );
+
+  grunt.registerTask("build:compressStandAlone",
+    "Compresses js to jasmine-all-min.js",
+    function() {
+      grunt.file.write(build_dir + "jasmine-all-min.js", compressJsFile(build_dir + "jasmine-all.js"));
+    }
+  );
+
+  grunt.registerTask("build",
+    "Builds and minifies jasmine-all-min.js",
+    [
+      "build:compileStandAlone",
+      "build:compressStandAlone"
+    ]
+  );
+
+  grunt.registerTask("clean",
+    "Cleans the build directory",
+    function() {
+      grunt.file.delete(build_dir);
+    }
+  );
+
+};
+
+/* vim:set ts=2 sw=2 et fdm=marker: */
